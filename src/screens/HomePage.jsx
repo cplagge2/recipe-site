f97cf68c6f89b343e82b920bc94cbb4926af9136
cplagge2 <cplagge@wisc.edu>
2024-01-13 17:40:18 -0600

    Updates

--- src/screens/HomePage.jsx
+++
@@ -1,8 +0,0 @@
-const HomePage = (props) => {
-    return <div>
-        <h1>Welcome!</h1>
-        <h4>Feel free to browse recipes or add some of your own!</h4>
-    </div>
-}
-
-export default HomePage;
